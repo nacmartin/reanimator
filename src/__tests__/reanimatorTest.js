b52ad42d4a132814283b95c50a6d996712b01914
Nacho Martín <nacho@limenius.com>
2017-11-09 20:29:03 +0100

    Can suspend

--- src/__tests__/reanimatorTest.js
+++ src/__tests__/reanimatorTest.js
@@ -52,9 +52,38 @@ describe('Reanimator', function() {
         })
 
         mockRaf.step({ count: 10 })
-        reanimator.cancel()
+        reanimator.suspend()
         mockRaf.step({ count: 10 })
         expect(cb.callCount).to.equal(10)
+
+    })
+    it('can resume an animation', function() {
+        let mockRaf = createMockRaf()
+        let reanimator = new Reanimator({raf: mockRaf.raf})
+        let cb = sinon.spy()
+        reanimator.setDispatcher(cb)
+
+        reanimator.setState({
+            streams: [ 'TIME' ],
+            outputs: [ 'x' ],
+            systems: [
+                {
+                    input: 'TIME',
+                    output: 'x',
+                    domain: [Date.now(), Date.now() + 4000],
+                    range: [100, 200],
+                    easing: 'easeLinear',
+                },
+            ]
+        })
+
+        mockRaf.step({ count: 10 })
+        reanimator.suspend()
+        mockRaf.step({ count: 10 })
+        expect(cb.callCount).to.equal(10)
+        reanimator.resume()
+        mockRaf.step({ count: 5 })
+        expect(cb.callCount).to.equal(15)
 
     })
 })
--- src/reanimator.js
+++ src/reanimator.js
@@ -1,37 +1,47 @@
-import defaultRaf from 'raf'
-import buildAnimation from './animation'
+import defaultRaf from "raf";
+import buildAnimation from "./animation";
 
 export default class Reanimator {
-    constructor(options) {
-        this.dispatcher = null
-        this.state = {}
-        this.animation = null
-        this.runAnimation = this.runAnimation.bind(this)
-        this.setState = this.setState.bind(this)
+    constructor(options = {}) {
+        this.dispatcher = null;
+        this.state = {};
+        this.animation = null;
+        this.runAnimation = this.runAnimation.bind(this);
+        this.setState = this.setState.bind(this);
         //this.setDispatcher = this.setDispatcher.bind(this)
-        this.raf = options.raf || defaultRaf
-        this.suspended = false
+        this.raf = options.raf || defaultRaf;
+        this.suspended = false;
 
-        this.raf(this.runAnimation)
+        this.raf(this.runAnimation);
     }
 
     setDispatcher(dispatcher) {
-        this.dispatcher = dispatcher
+        this.dispatcher = dispatcher;
     }
 
     setState(state) {
-        this.state = state
-        this.animation = buildAnimation(state)
+        this.state = state;
+        this.animation = buildAnimation(state);
     }
 
     suspend() {
-        this.suspended = true
+        this.suspended = true;
+    }
+
+    resume() {
+        this.suspended = false;
+        this.raf(this.runAnimation);
     }
 
     runAnimation() {
         if (!this.suspended) {
-            this.dispatcher && this.animation && this.dispatcher({type: 'REANIMATOR/ANIMATE', properties: {...this.animation.next().value}})
-            this.raf(this.runAnimation)
+            this.dispatcher &&
+                this.animation &&
+                this.dispatcher({
+                    type: "REANIMATOR/ANIMATE",
+                    properties: { ...this.animation.next().value }
+                });
+            this.raf(this.runAnimation);
         }
     }
 }
